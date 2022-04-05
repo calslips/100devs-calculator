78750d9364f32ba46ad33c007cd0902934be1f5e
Caleb Phillips <csphillips5@gmail.com>
2022-04-05 17:30:56 -0400

    implement ability for user to interact with calculator via key presses

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ class Calculator {
     const display = document.querySelector('span');
     const buttons = Array.from(document.querySelectorAll('button'));
 
+    display.innerText = '0';
+    let target = ''
     let firstOperand = '';
     let operator = '';
     let secondOperand = '';
@@ -13,52 +15,92 @@ class Calculator {
       '-': (a, b) => a - b,
     }
 
-    this.getOperand = (e) => {
-      if (!operator) {
-        if (e.target.innerText === '-' && firstOperand && !secondOperand) {
-          // ignore minus sign while first operand is in process of being made, but allow it to go through if it's the first character
-        } else {
-          firstOperand += e.target.innerText
-          display.innerText = firstOperand
+    this.handleNumPress = (key) => {
+      const approvedNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-'];
+      return approvedNums.includes(key) ? key : '';
+    }
+
+    this.handleOpPress = (key) => {
+      const approvedOps = ['/', 'x', '+', '-'];
+      return approvedOps.includes(key) ? key : '';
+    }
+
+    this.determineInputType = (e, numOrOp) => {
+      if (e.type === 'click') {
+        target = e.target.innerText;
+      } else if (e.type === 'keydown') {
+        if (numOrOp === 'num') {
+          target = this.handleNumPress(e.key)
+        } else if (numOrOp === 'op') {
+          target = this.handleOpPress(e.key);
         }
-      } else {
-        if (e.target.innerText === '-' && secondOperand) {
-          // ignore minus sign while in process of creating second opperand unless its the first char
+      }
+    }
+
+    this.getOperand = (e) => {
+      this.determineInputType(e, 'num');
+// PROBLEM HERE? switching from clicks to keypress in the middle of an operation, target is defined so it doubles up on something....?
+// but when only clicking OR only pressing keys, no issue
+      if (target) {
+        if (!operator) {
+          if (target === '-' && firstOperand && !secondOperand) {
+            // ignore minus sign while first operand is in process of being made, but allow it to go through if it's the first character
+          } else {
+            firstOperand += target
+            display.innerText = firstOperand
+          }
         } else {
-          secondOperand += e.target.innerText
-          display.innerText = `${firstOperand} ${operator} ${secondOperand}`
+          if (target === '-' && secondOperand) {
+            // ignore minus sign while in process of creating second opperand unless its the first char
+          } else {
+            secondOperand += target
+            display.innerText = `${firstOperand} ${operator} ${secondOperand}`
+          }
         }
       }
     }
     this.getOperator = (e) => {
+      this.determineInputType(e, 'op');
+
       if (firstOperand === '-') {
         // skip if minus was meant to be a negative value and NOT subtraction
       } else if (!operator && firstOperand) {
-        operator = e.target.innerText;
+        operator = target;
         display.innerText = `${firstOperand} ${operator}`
       }
     }
-    this.getResult = () => {
-      if (firstOperand && operator && secondOperand) {
-        let result = operations[operator](+firstOperand, +secondOperand);
-        display.innerText = result;
-        firstOperand = result;
-        operator = '';
-        secondOperand = '';
+    this.getResult = (e) => {
+      if (e.type === 'click' || e.key === 'Enter') {
+        if (firstOperand && operator && secondOperand) {
+          let result = operations[operator](+firstOperand, +secondOperand);
+          display.innerText = result;
+          firstOperand = result;
+          operator = '';
+          secondOperand = '';
+        }
       }
     }
-    this.reset = () => {
-      firstOperand = '';
-      operator = '';
-      secondOperand = '';
-      display.innerText = '';
+    this.reset = (e) => {
+      if (e.type === 'click' || e.key === 'Backspace') {
+        firstOperand = '';
+        operator = '';
+        secondOperand = '';
+        display.innerText = '0';
+      }
     }
 
+    // click event listeners
     display.addEventListener('click', this.reset);
     buttons.forEach(btn => btn.addEventListener('click', this.collectInput))
     buttons.filter(btn => btn.classList.value.includes('num')).forEach(btn => btn.addEventListener('click', this.getOperand))
     buttons.filter(btn => btn.classList.value.includes('op')).forEach(btn => btn.addEventListener('click', this.getOperator))
     document.querySelector('.equal').addEventListener('click', this.getResult)
+
+    // typed event listeners
+    document.addEventListener('keydown', this.getOperand);
+    document.addEventListener('keydown', this.getOperator);
+    document.addEventListener('keydown', this.getResult);
+    document.addEventListener('keydown', this.reset);
   }
 }
 
